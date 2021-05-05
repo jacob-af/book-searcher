88958d172b3a0b136de1ac0d461e340c6d552893
jacob-af <jfeitler@gmail.com>
2021-05-05 16:45:14 -0400

    nav now working

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div>
-      <p>nav</p>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <Link class="navbar-brand" to="/">
+          Google Book Search
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <Link class="nav-link " aria-current="page" to="/">
+                Search
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/saved">
+                Saved
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
